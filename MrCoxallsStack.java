8d43ac5367f91f67cef4e3a01f7cd8b6cb4afc3b
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-12-15 15:07:13 -0500

    Update MrCoxallsStack.java

--- MrCoxallsStack.java
+++ MrCoxallsStack.java
@@ -7,11 +7,23 @@ public class MrCoxallsStack {
 	public static void main(String args[]){
 	Scanner userInput = new Scanner(System.in);
 	Stack stackNum = new Stack();
+	
+	do{
 	System.out.println("Enter a number to be placed on the Stack");
 	int userNum = userInput.nextInt();
-	stackNum.push(userNum);
-	stackNum.pop(0);
-	stackNum.push(userNum);
-	stackNum.print();
+	if (userNum >= 0){
+		stackNum.push(userNum);
+	}else if (userNum == -1){
+		stackNum.pop(1);
+	}else{
+		break;
 	}
+		
+
+	stackNum.print();
+	
+	}while(true);
+	
+ }
 }
+	
